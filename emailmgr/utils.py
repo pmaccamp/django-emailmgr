687fa56f212a098285d7f41726b574707a0598a0
Riley <rjwgibbs@gmail.com>
2019-10-17 13:39:50 -0700

    Encode before hashing

--- emailmgr/utils.py
+++ emailmgr/utils.py
@@ -19,7 +19,7 @@ else:
 def get_unique_random(length=10):
     randtime = str(time.time()).split('.')[0]
     rand = ''.join([random.choice(randtime+string.ascii_letters+string.digits) for i in range(length)])
-    return sha1(rand).hexdigest()[:length]
+    return sha1(rand.encode('utf-8')).hexdigest()[:length]
  
 # given a template name, return its path
 def get_template(name):
